3bc5cf2fa32528c62b3f91526690561d243b0a70
Raghu Teja.B <rteja.bits@gmail.com>
2013-03-24 10:50:11 +0530

    1. Bellman Ford - Yen's modification implemented (Yet to be tested)

--- Graph/src/bellmanford.cpp
+++ Graph/src/bellmanford.cpp
@@ -24,7 +24,6 @@ bool relax(vertex *u, vertex *v, int edge_cost)
 {
      assert(u);
      assert(v);
-     assert(edge_cost >= 0);
 
      if (v->d > (u->d + edge_cost))
      {
@@ -38,11 +37,15 @@ bool relax(vertex *u, vertex *v, int edge_cost)
 //Standard BellmanFord implementation which is of O(VE) complexity.
 bool BellmanFord(graph *g, vertex *s)
 {
+     assert(g);
+     assert(s);
+
      initialize(g, s);
      
      unsigned int it = 0;
      for (it = 0; it < vsize() - 1; it++)
      {
+	  //Relax all edges of the graph
 	  unsigned int is = 0;
 	  for (is = 0; is < vsize(); is++)
 	  {
@@ -59,6 +62,8 @@ bool BellmanFord(graph *g, vertex *s)
 	  }
      }
 
+     // If there are edges that can be relaxed still 
+     // there is a negative weight cycle.
      for (it = 0; it < vsize(); it++)
      {
 	  vertex *u = g->get_vertex(it);
@@ -85,9 +90,80 @@ bool BellmanFord(graph *g, std::vector<vertex*> s)
      return true;
 }
 
-
 //BellmanFord Algorithm Yen's Modification.
 bool BellmanFord_Yen(graph *g, vertex *s)
 {
+     assert(g);
+     assert(s);
+
+     initialize(g, s);
      
+     unsigned int it = 0;
+     unsigned int max_it = (vsize() % 2) ? (vsize() / 2) : ((vsize() / 2) + 1);
+
+     
+     for (it = 0; it < max_it; it++)
+     {
+	  //Relaxation of edges i->j where i < j
+	  unsigned int is = 0;
+	  for (is = 0; is < vsize(); is++)
+	  {
+	       vertex *u = g->get_vertex(is);
+	       assert(u);
+	       
+	       unsigned int iu = 0;
+	       for (iu = 0; iu < u->edges.size(); iu++);
+	       {
+		    assert(u->edges[iu]);
+
+		    if (ends[1] <= is)
+			 continue;
+
+		    vertex *v = vertices[u->edges[iu]->ends[1]];
+		    relax(u, v, u->edges[iu]->cost);
+	       }
+	  }
+	  
+	  //Relaxation of edges i->j where i > j
+	  for (is = 0; is < vsize(); is++)
+	  {
+	       vertex *u = g->get_vertex(is);
+	       assert(u);
+	       
+	       unsigned int iu = 0;
+	       for (iu = 0; iu < u->edges.size(); iu++);
+	       {
+		    assert(u->edges[iu]);
+
+		    if (ends[1] >= is)
+			 continue;
+
+		    vertex *v = vertices[u->edges[iu]->ends[1]];
+		    relax(u, v, u->edges[iu]->cost);
+	       }
+	  }
+	
+
+     }
+  
+
+     // If there are edges that can be relaxed still 
+     // there is a negative weight cycle.
+     for (it = 0; it < vsize(); it++)
+     {
+	  vertex *u = g->get_vertex(it);
+	  
+	  unsigned int is = 0;
+	  for (is = 0; is < u->edges.size(); is++)
+	  {
+	       assert(u->edges[iu]);
+	       vertex *v = vertices[u->edges[iu]->ends[1]];
+	       if (relax(u, v, u->edges[iu]->cost))
+		   return false;
+	       
+	  }
+
+     }	  
+	  
+     return true;
 }
